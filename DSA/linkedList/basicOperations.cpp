6b7815fa6cb3a529c58ef3ef7fdb7eb72aaa204f
Mazharul Alam <mazharulalam26@gmail.com>
2023-09-21 17:16:26 +0600

    linked list converted to Class

--- DSA/linkedList/basicOperations.cpp
+++ DSA/linkedList/basicOperations.cpp
@@ -1,158 +1,144 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct node
+class Node
 {
-    node *prev;
+public:
     int data;
-    node *next;
+    Node *next;
+    Node *prev;
 };
-node *head = NULL;
-node *tail = NULL;
 
-void push(int val)
+class List
 {
-    if (tail != NULL)
+public:
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    void push(int val)
     {
-        node *nn = new node();
-        nn->data = val;
-        nn->next = NULL;
-        nn->prev = tail;
-        tail->next = nn;
-        tail = tail->next;
+        if (tail != NULL)
+        {
+            Node *nn = new Node();
+            nn->data = val;
+            nn->next = NULL;
+            nn->prev = tail;
+            tail->next = nn;
+            tail = tail->next;
+        }
+        else
+        {
+            head = new Node();
+            head->data = val;
+            head->next = NULL;
+            head->prev = NULL;
+            tail = head;
+        }
     }
-    else
+
+    void pop()
     {
-        head = new node();
-        head->data = val;
-        head->next = NULL;
-        head->prev = NULL;
-        tail = head;
+        Node *tmp = tail;
+        tail = tail->prev;
+        delete (tmp);
     }
-}
 
-void pop()
-{
-    node *tmp = tail;
-    tail = tail->prev;
-    delete (tmp);
-}
-
-void append(int val)
-{
-    if (head != NULL)
+    void append(int val)
     {
-        node *nn = new node();
-        nn->data = val;
-        nn->next = head;
-        nn->prev = NULL;
-        head = nn;
+        if (head != NULL)
+        {
+            Node *nn = new Node();
+            nn->data = val;
+            nn->next = head;
+            nn->prev = NULL;
+            head = nn;
+        }
+        else
+        {
+            head = new Node();
+            head->data = val;
+            head->next = NULL;
+            head->prev = NULL;
+            tail = head;
+        }
     }
-    else
-    {
-        head = new node();
-        head->data = val;
-        head->next = NULL;
-        head->prev = NULL;
-        tail = head;
-    }
-}
 
-void shift()
-{
-    node *tmp = head;
-    head = head->next;
-    delete (tmp);
-}
-
-void traverse()
-{
-    node *trav = head;
-    while (trav != NULL)
+    void shift()
     {
-        cout << trav->data << " ";
-        trav = trav->next;
-    }
-}
-
-void traverseReverse()
-{
-    node *trav = tail;
-    while (trav != NULL)
-    {
-        cout << trav->data << " ";
-        trav = trav->prev;
-    }
-}
-
-void delet()
-{
-    while (head->next != NULL)
-    {
-        node *tmp = head;
+        Node *tmp = head;
         head = head->next;
         delete (tmp);
     }
-}
 
-void insert(int x, int n)
-{
-    node *cn = head;
-    node *nn = new node();
-    nn->data = x;
-    if (n > 1)
+    void traverse()
     {
-        for (int i = 0; i < n - 2; i++)
+        Node *trav = head;
+        while (trav != NULL)
         {
-            if (cn->next != NULL)
-            {
-                cn = cn->next;
-            }
-            else
-            {
-                cout << "Out of bound!";
-                return;
-            }
+            cout << trav->data << " ";
+            trav = trav->next;
         }
-        nn->next = cn->next;
-        nn->prev = cn->prev;
-        cn->next = nn;
-        cn = cn->next;
     }
-    else
+
+    void traverseReverse()
     {
-        nn->next = head;
-        nn->prev = NULL;
-        head = nn;
+        Node *trav = tail;
+        while (trav != NULL)
+        {
+            cout << trav->data << " ";
+            trav = trav->prev;
+        }
     }
-}
+
+    void delet()
+    {
+        while (head->next != NULL)
+        {
+            Node *tmp = head;
+            head = head->next;
+            delete (tmp);
+        }
+    }
+
+    void insert(int x, int n)
+    {
+        Node *cn = head;
+        Node *nn = new Node();
+        nn->data = x;
+        if (n > 1)
+        {
+            for (int i = 0; i < n - 2; i++)
+            {
+                if (cn->next != NULL)
+                {
+                    cn = cn->next;
+                }
+                else
+                {
+                    cout << "Out of bound!";
+                    return;
+                }
+            }
+            nn->next = cn->next;
+            nn->prev = cn->prev;
+            cn->next = nn;
+            cn = cn->next;
+        }
+        else
+        {
+            nn->next = head;
+            nn->prev = NULL;
+            head = nn;
+        }
+    }
+};
 
 int main()
 {
+    List list = List();
     for (int i = 0; i < 5; i++)
     {
-        push(i);
+        list.push(i);
     }
-    // traverse();
-    // cout << '\n';
-    // traverseReverse();
-    // cout << '\n';
-    // append(-1);
-    // traverse();
-    // cout << '\n';
-    // pop();
-    // traverse();
-    // cout << '\n';
-    // push(11);
-    traverse();
-    cout << '\n';
-    // shift();
-    // traverseReverse();
-    cout << '\n';
-    insert(33,3);
-    insert(44,3);
-    insert(55,3);
-    insert(66,3);
-    traverse();
-    // delet();
+    list.traverse();
 }
