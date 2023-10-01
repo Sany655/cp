b430cb971417530dc1ff58de9925e22725c5d8e9
Mazharul Alam <mazharulalam26@gmail.com>
2023-10-01 18:50:18 +0600

    tree basics complete

--- DSA/tree/BinarySearchTree.cpp
+++ DSA/tree/BinarySearchTree.cpp
@@ -120,28 +120,123 @@ public:
             return true;
     }
 
-    void delete_iterative(int v)
+    bool delete_iterative(int v)
     {
         Node *cn = root;
-        Node *dn,*rn;
+        int i = 0;
+        bool isD = false;
         while (cn != NULL)
         {
+            i++;
             if (cn->value < v)
                 cn = cn->r;
             else if (cn->value > v)
                 cn = cn->l;
             else if (cn->value == v)
             {
-                dn = cn;
-                if(cn->l != NULL)
-                    rn = cn->l;
-                else rn = cn->r;
-                while (cn!=NULL)
+                isD = true;
+                Node *dn = cn;
+                cout << "-- found " << cn->value << " at level " << i << endl;
+                if (cn->l == NULL && cn->r != NULL)
                 {
-                    
+                    cout << "in first condition *\n";
+                    if (root == cn)
+                        root = cn->r;
+                    else
+                    {
+                        cn->r->parent = cn->parent;
+                        cn->parent->l = cn->r;
+                    }
+                    delete (dn);
                 }
-                
+                else if (cn->l != NULL && cn->r == NULL)
+                {
+                    cout << "in second condition **\n";
+                    if (root == cn)
+                        root = cn->l;
+                    else
+                    {
+                        cn->l->parent = cn->parent;
+                        cn->parent->r = cn->l;
+                    }
+                    delete (dn);
+                }
+                else if (cn->l != NULL && cn->r != NULL)
+                {
+                    cout << "in third condition ***\n";
+                    Node *successor = cn->r;
+                    while (successor->l != NULL)
+                        successor = successor->l;
+                    cn->value = successor->value;
+                    if (successor->parent != cn)
+                        successor->parent->l = successor->r;
+                    else
+                        successor->parent->r = successor->l;
+                    delete successor;
+                }
+                else
+                {
+                    cout << "at last condition ****\n";
+                    if (cn->parent->r == cn)
+                        cn->parent->r = NULL;
+                    else
+                        cn->parent->l = NULL;
+                    delete (cn);
+                }
+                break;
             }
+        }
+        if (!isD)
+            return false;
+        else
+            return true;
+    }
+
+    Node *delete_recursive(int v, Node *cn)
+    {
+        if (cn == NULL)
+            return cn;
+        if (cn->value > v)
+        {
+            cn->l = delete_recursive(v, cn->l);
+            return cn;
+        }
+        else if (cn->value < v)
+        {
+            cn->r = delete_recursive(v, cn->r);
+            return cn;
+        }
+
+        if (cn->l == NULL)
+        {
+            Node *tmp = cn->r;
+            delete cn;
+            return tmp;
+        }
+        else if (cn->r == NULL)
+        {
+            Node *tmp = cn->l;
+            delete cn;
+            return tmp;
+        }
+        else
+        {
+            Node *succParent = cn;
+            Node *succ = cn->r;
+            while (succ->l != NULL)
+            {
+                succParent = succ;
+                succ = succ->l;
+            }
+            if (succParent != cn)
+                succParent->l = succ->r;
+            else
+                succParent->r = succ->r;
+
+            cn->value = succ->value;
+
+            delete succ;
+            return root;
         }
     }
 };
@@ -157,12 +252,22 @@ int main()
     f.bst_push(115);
     f.bst_push(90);
     f.bst_push(0);
+    f.bst_push(118);
+    f.bst_push(116);
+    f.bst_push(105);
+    f.bst_push(110);
+    f.bst_push(119);
+    f.bst_push(117);
+
     // cout << "pre order : ";
     // f.preOrder(f.root);
-    cout << "\nin order : ";
-    f.inOrder(f.root);
+
+    // cout << "\nin order : ";
+    // f.inOrder(f.root);
+
     // cout << "\npost order : ";
     // f.postOrder(f.root);
+
     // cout << "\nwhat do you want to search? ";
     // int x;
     // cin >> x;
@@ -171,10 +276,30 @@ int main()
     //     cout << "Yes";
     // else
     //     cout << "No";
-    cout << "\nwhat do you want to delete? ";
-    int x;
-    cin >> x;
-    f.delete_iterative(x);
-    cout << "\nin order : ";
+
+    /* test binary search tree delete a node*/
     f.inOrder(f.root);
+    while (true)
+    {
+        cout << "\nwhat do you want to delete? ";
+        int x;
+        cin >> x;
+        if (!f.delete_iterative(x))
+            break;
+        f.inOrder(f.root);
+    }
+
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     f.bst_push(i);
+    // }
+    // f.inOrder(f.root);
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     cout << endl
+    //          << i << "th times\n";
+    //     f.delete_iterative(i);
+    //     // Node *r = f.delete_recursive(i, f.root);
+    //     f.inOrder(f.root);
+    // }
 }
--- DSA/tree/tempCodeRunnerFile.cpp
+++ DSA/tree/tempCodeRunnerFile.cpp
@@ -1,1 +1,10 @@
-cn
+f.inOrder(f.root);
+    while (true)
+    {
+        cout << "\nwhat do you want to delete? ";
+        int x;
+        cin >> x;
+        if (!f.delete_iterative(x))
+            break;
+        f.inOrder(f.root);
+    }
